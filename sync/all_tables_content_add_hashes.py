2878763911a90b5a774ad5181eed41ea97064263
Matthias Bernt <m.bernt@ufz.de>
2026-07-15 20:15:54 +0200

    document design decision

--- sync/all_tables_content_add_hashes.py
+++ sync/all_tables_content_add_hashes.py
@@ -83,7 +83,10 @@ def iter_matching_files(pathspec):
         )
 
     else:
-        # Treat final component as a filename prefix
+        # this is in most cases a prefix. we still take all files contained
+        # in the parent directory. this is because for instance the blast databases
+        # also contain files, taxdb.btd, taxdb.bti and taxonomy4blast.sqlite3
+        # which do not match the prefix
         parent = p.parent
         prefix = p.name
 
