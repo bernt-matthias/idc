9e6cb137e49e3c9f3fc81e8950a5be72863a62b8
Lucille Delisle <lucille.delisle@unige.ch>
2026-08-14 08:09:25 +0200

    fix position of counter

--- sync/all_fasta_files_to_refget_store.py
+++ sync/all_fasta_files_to_refget_store.py
@@ -131,15 +131,16 @@ def import_fasta_all(
         if store is None:
             store = RefgetStore.in_memory()
 
+        logger.info(
+            f"Processing fasta {i + 1}/{len(fasta_all)}."
+        )
+
         try:
             collection, new = store.add_sequence_collection_from_fasta(local_fasta_path)
         except Exception as e:
             logger.info(f"Could not load {local_fasta_path}: {e}")
             continue
 
-        logger.info(
-            f"Processing fasta {i + 1}/{len(fasta_all)}."
-        )
         add_galaxy_aliases_to_store(store, collection, fasta_record)
 
         refget_metadata_blob = {
